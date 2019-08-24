958a72cfa8b4a4dc8df70d362db5e8c64d19b93e
SemteulGaram <scgtdy7151@gmail.com>
2019-08-25 01:02:54 +0900

    Add: RemoteScriptExecute.js - Log alias

--- _personal/RemoteScriptExecute/RemoteScriptExecute.js
+++ _personal/RemoteScriptExecute/RemoteScriptExecute.js
@@ -1,13 +1,13 @@
 const scriptName = "RemoteScriptExecute.js";
-const VERSION = 'v1.2';
+const VERSION = 'v1.3';
 
 const config = {
   targetRoom: 'EXAMPLE_TARGET',
   commandExecute: '/rse',
   evalPrefix: 'var _debug=Log.debug,_info=Log.info,_error=Log.error;'
-    + 'Log.debug=function(){sendChat(Array.prototype.join.call(arguments, " "));_debug.apply(Log, arguments)};'
-    + 'Log.info=function(){sendChat(Array.prototype.join.call(arguments, " "));_info.apply(Log, arguments)};'
-    + 'Log.error=function(){sendChat(Array.prototype.join.call(arguments, " "));_error.apply(Log, arguments)};'
+    + 'Log.debug=function(){sendChat(Array.prototype.join.call(arguments, " "));_debug.apply(Log, arguments)};Log.d=Log.debug;'
+    + 'Log.info=function(){sendChat(Array.prototype.join.call(arguments, " "));_info.apply(Log, arguments)};Log.i=Log.info;'
+    + 'Log.error=function(){sendChat(Array.prototype.join.call(arguments, " "));_error.apply(Log, arguments)};Log.e=Log.error;'
 };
 
 function sendChat(msg) {
